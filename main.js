3ffc7d426619b0cdc0dd6f61f1467fc6f7a812f4
= <dallitossantos1@hotmail.com>
2022-09-11 15:35:06 -0300

    replaced ' for "

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
+const {app, BrowserWindow, ipcMain } = require("electron")
+const path = require("path")
 const { getLanguagesJSON } = require("./languages/languages");
 const { getMangaByName, getChapterByMangaURL, getMangaFilesByChapter } = require("./manga_finder/manga_finder");
 const { downloadMangaIntoDirectory } = require("./manga_finder/manga_downloader");
@@ -14,7 +14,7 @@ function createWindow () {
     width: 1280,
     height: 720,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, "preload.js")
     }
   })
 
@@ -30,7 +30,7 @@ function createWindow () {
 
   ipcMain.handle("get-languages", (event, args) => getLanguagesJSON());
 
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile("index.html")
 
   win = mainWindow;
 
@@ -45,19 +45,19 @@ app.whenReady().then(() => {
 
   createWindow()
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
+  app.on("activate", function () {
+    // On macOS it"s common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
+// Quit when all windows are closed, except on macOS. There, it"s common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
+// In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
